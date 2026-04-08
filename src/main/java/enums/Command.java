8c26acb4f2add9a6fe92ef12578225d2292b7849
okanakdag <mertakdag08@gmail.com>
2026-04-09 00:25:24 +0300

    add maxArgs to Command

--- src/main/java/enums/Command.java
+++ src/main/java/enums/Command.java
@@ -1,20 +1,22 @@
 package enums;
 
 public enum Command {
-    ADD("add", 2),
-    UPDATE("update", 2),
-    DELETE("delete", 2),
-    LIST("list", 1),
-    MARK_IN_PROGRESS("mark-in-progress", 2),
-    MARK_DONE("mark-done", 2),
-    HELP("help", 1);
+    ADD("add", 2, 2),
+    UPDATE("update", 2, 2),
+    DELETE("delete", 2, 2),
+    LIST("list", 1, 2),
+    MARK_IN_PROGRESS("mark-in-progress", 2, 2),
+    MARK_DONE("mark-done", 2, 2),
+    HELP("help", 1, 1);
 
     private final String description;
     private final int minArgs;
+    private final int maxArgs;
 
-    Command(String description, int minArgs) {
+    Command(String description, int minArgs, int maxArgs) {
         this.description = description;
         this.minArgs = minArgs;
+        this.maxArgs = maxArgs;
     }
 
     public String getDescription() {
@@ -22,7 +24,11 @@ public enum Command {
     }
 
     public int getMinArgs() {
-       return minArgs;
+        return minArgs;
+    }
+
+    public int getMaxArgs() {
+        return maxArgs;
     }
 
     public static Command fromString(String text) {
